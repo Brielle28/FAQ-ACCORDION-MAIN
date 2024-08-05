3aaacd372b3d978b7e2ebe499b53b7d7cc45d332
Brielle28 <chukwuemerieclare@gmail.com>
2024-08-05 13:54:45 +0100

    acordion functionalities and design

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "../../src/App.css"
 
 const Main = () => {
   return (
     <>
       <div className="bg-white px-10 py-5 rounded-[17px]">
         <div className="flex items-start justify-start gap-2">
-          <img src="/images/icon-star.svg" alt="star" />
-          <h5 className="text-[32px] text-black font-mono font-semibold">FAG'S</h5>
+          <img src="/images/icon-star.svg" alt="star" className="mt-3 size-[50px]" />
+          <h5 className="text-[50px] text-black font-WorkSans font-bold">FAQs</h5>
         </div>
         <div>
-          <div className="collapse collapse-plus bg-white text-black border-b-[1.2px] rounded-none border-gray-500">
+          <div className="collapse  bg-white text-black border-b-[1.2px] rounded-none border-gray-300">
             <input type="radio" name="my-accordion-2" defaultChecked />
             <div className="collapse-title text-xl font-medium">
               Click to open this one and close others
@@ -18,7 +19,7 @@ const Main = () => {
               <p>hello</p>
             </div>
           </div>
-          <div className="collapse collapse-plus bg-white text-black border-b-[1.5px] rounded-none border-gray-500">
+          <div className="collapse  bg-white text-black border-b-[1.2px] rounded-none border-gray-300">
             <input type="radio" name="my-accordion-2" defaultChecked />
             <div className="collapse-title text-xl font-medium">
               Click to open this one and close others
@@ -27,7 +28,7 @@ const Main = () => {
               <p>hello</p>
             </div>
           </div>
-          <div className="collapse collapse-plus bg-white text-black border-b-[1.5px] rounded-none border-gray-500">
+          <div className="collapse  bg-white text-black border-b-[1.2px] rounded-none border-gray-300">
             <input type="radio" name="my-accordion-2" defaultChecked />
             <div className="collapse-title text-xl font-medium">
               Click to open this one and close others
